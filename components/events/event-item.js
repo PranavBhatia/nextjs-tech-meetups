485fa80aa4a68fb58efc5432faa62b7a8cfff71e
Pranav Bhatia <bhatiapranav1996@gmail.com>
2021-09-10 00:44:07 -0400

    Adding Regular React Components

---
+++ components/events/event-item.js
@@ -0,0 +1,5 @@
+export default function EventItem(props) {
+  const { item } = props;
+
+  return <li></li>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import { getFeaturedEvents } from "../dummy-data";
+
 export default function Home() {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-      <h1>The Home Page</h1>
+      <ul>
+        
+      </ul>
     </div>
   );
 }
